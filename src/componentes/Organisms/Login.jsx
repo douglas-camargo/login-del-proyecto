f1ee6080b12fd15536784843620bc3e9a2e3d3de
douglas camargo <douglascamargo159@gmail.com>
2020-11-02 10:59:14 -0430

    valida con onChange, onSubmit y si no se cumplen las condiciones el boton estara desabilitado

--- src/componentes/Organisms/Login.jsx
+++ src/componentes/Organisms/Login.jsx
@@ -2,79 +2,112 @@ import React from 'react'
 import {validarNumero} from '../../funciones/Funciones.js'
 
 const validate = values => {
- 
+
     const errors = {}
     if(!values.rut){
-        errors.rut = alert('Te falto llenar el campo Rut')
+        errors.rut = 'Te falto llenar el campo Rut'
+        }
+    else if(values.rut.length < 9 || !validarNumero(values.rut)){
+        errors.rut = ''
     }
-    else if(values.rut.length < 9){
-        errors.rut = alert('Recuerde introducir un número de rut valido, mínimo 9 digitos.')
+    if(!values.pass){
+        errors.pass = 'Te falto llenar el campo Contraseña'
     }
-    else if(!validarNumero(values.rut)){
-        errors.rut = alert('recuerde que solo puede ingresar valores numericos.')
+    else if(values.pass.length < 5){
+        errors.pass = ''
     }
-
-    if(!values.password){
-        errors.password = alert('Te falto llenar el campo Contraseña')
-    }
-    
     return errors
-}
-
+    }
 class Login extends React.Component {
     constructor(props){
         super(props)
 
         this.state = {
-            errors: {}   
+            errors: {},
+            msgErrorRut:"",
+            msgErrorPassword:"",
+            rut:"",
+            pass:"",
+            desabilitarBtn: true,
+            desabilitarBtne: true
         }
-
-         this.handleSubmit = this.handleSubmit.bind(this);
-         this.handleChange = this.handleChange.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
 
-handleChange = ({ target}) => {
+handleChange = ({target, e}) => {    
     const {name, value} = target
-    this.setState({ [name]: value })
+    this.setState({ [name]: value
+    })
+
+    if(name === "rut"){
+    if(target.value.length < 9 || !validarNumero(target.value) || target.value === "") {
+        this.setState({
+            msgErrorRut:"Recuerde introducir un número de teléfono valido, mínimo 9 digitos.",
+            desabilitarBtn: true,
+    })
+    } 
+    else {
+        this.setState({
+            msgErrorRut:"",
+            desabilitarBtn: false,
+        })
+    }}
+
+    if(name === "pass"){
+    if(target.value === "" || target.value.length < 5 ) {
+        this.setState({
+            msgErrorPassword:"La contraseña tiene que ser mayor a 5 caracteres",
+            desabilitarBtne: true,
+    })
     }
-
-
-handleSubmit(event) {
-    event.preventDefault()
+    
+    else { 
+        this.setState({
+            msgErrorPassword:"",
+            desabilitarBtne: false,
+        })
+    }}
+    }
+    
+handleSubmit = e => {
+    e.preventDefault()
     const {errors, ...sinErrors} = this.state
     const result = validate(sinErrors)
-
     this.setState({errors: result})
     if (!Object.keys(result).length){
         console.log('gracias por llenar el formulario')
-
     }
 }
 
 render() {
-    const {errors} = this.state
+    const {errors, msgErrorRut, msgErrorPassword} = this.state
     return (
         <form action="/hola" className="login" onSubmit={this.handleSubmit}>
             <div className="login-screen">
                 <img className="imagen" src="./imagenes/foto.png" alt="Login" />
                 <div className="app-title">
-                    <h1>Login</h1>
+                    <h1>Inicio de sesión</h1>
                 </div>
 
                 <div className="login-form">
                     <div className="control-group">
                     <input type="text" name="rut" onChange={this.handleChange} className="login-field" placeholder="Rut" id="login-name" />
-                    {errors.rut && <span>{errors.rut}</span>}
+                    {errors.rut 
+                    ?<span className="tamañoDeLetraSpan">{errors.rut}</span>
+                    :<span className="tamañoDeLetraSpan">{msgErrorRut}</span> }
                     <label className="login-field-icon fui-user" for="login-name"></label>
                     </div>
     
                     <div className="control-group">
-                    <input type="password" name="password" onChange={this.handleChange} className="login-field" placeholder="Contraseña" id="login-pass" />
-                    {errors.password && <span>{errors.password}</span>}
+                    <input type="password" name="pass" onChange={this.handleChange} className="login-field" placeholder="Contraseña" id="login-pass" />
+                    {errors.pass 
+                    ?<span className="tamañoDeLetraSpan">{errors.pass}</span>
+                    :<span className="tamañoDeLetraSpan">{msgErrorPassword}</span> }
                     <label className="login-field-icon fui-lock" for="login-pass"></label>
                     </div>
-                    <input type="submit" value="Entrar" className="center btn" /> 
-                    <a className="login-link" href="#">Lost your password?</a>
+                    <input type="submit" value="Entrar" className="center btn" 
+                    disabled={this.state.desabilitarBtn || this.state.desabilitarBtne} />   
+                    <a className="login-link" href="#">¿Olvido su contraseña?</a>
                 </div>
             </div>
         </form>
@@ -83,16 +116,4 @@ render() {
 }
 
 export default Login
-// function validar() {
-//     var elemento = document.getElementById("login-name").value
-//   if (elemento == ""){
-//     alert("Debes llenar el campo")
-//     return false
-//   }else {
-//     alert("Gracias por llenar esto ")
-//     return false
-//   }
-// }
-
-
 
